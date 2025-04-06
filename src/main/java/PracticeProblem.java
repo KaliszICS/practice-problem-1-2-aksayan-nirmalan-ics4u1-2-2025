59e956953387fd5056f77de71da45f7843b87469
aksayan nirmalan-ics4u1-2 <aksayan.nirmalan@student.tdsb.on.ca>
2025-04-06 18:36:20 +0000

    save changes

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,30 @@
 public class PracticeProblem {
-
-	public static void main(String args[]) {
-
-	}
-
 	
-
+	public static boolean validIndex(int[]array,int index) {
+		try{
+			int data = array[index];
+			return true;
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+	public static int divide(int first,int second){
+		try{
+			int value= first/second;
+			return value;
+		}
+		catch(ArithmeticException f) {
+			return 0;
+		}
+	}
+	public static int safeConvertStringtoInt(String String){
+		try{
+			int num1=Integer.parseInt(String);
+			return num1;
+		}
+		catch(NumberFormatException g) {
+			return 0;
+		}
+	}
 }
